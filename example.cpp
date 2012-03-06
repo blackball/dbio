73168086e0cb76e739d75f042a0294332d6a7cb8
blackball <bugway@gmail.com>
2012-03-06 17:26:20 +0800

    update formate

--- example.cpp
+++ example.cpp
@@ -25,11 +25,11 @@ int test_pack(void)
   }
   
   for(int i = 0; i < 1000000; ++i)
-  if (0 != pack_arr(dbase, arr, len))
-  {
-    fprintf(stderr, "unknow error!\n");
-    return 0;
-  }
+    if (0 != pack_arr(dbase, arr, len))
+    {
+      fprintf(stderr, "unknow error!\n");
+      return 0;
+    }
   
   writedb(dbase);
   return 0;
@@ -52,7 +52,7 @@ int test_fast_unpack(void)
   }
   
   for(int i = 0; i < 10; ++i)
-	 printf("%d ", arr[i]);
+    printf("%d ", arr[i]);
 
   fast_closedb(dbase);
   return 0;
@@ -75,7 +75,7 @@ int test_unpack(void)
   }
   
   for(int i = 0; i < 10; ++i)
-	 printf("%d ", arr[i]);
+    printf("%d ", arr[i]);
 
   closedb(dbase);
   return 0;
@@ -83,91 +83,91 @@ int test_unpack(void)
 
 void test_gallery()
 {
-	// Create a item
-	GItem *items[5];
-	int j;
-	for (j = 0; j < 5; ++j)
-		items[j] = new_gitem(4800);
+  // Create a item
+  GItem *items[5];
+  int j;
+  for (j = 0; j < 5; ++j)
+    items[j] = new_gitem(4800);
 	
-	int i = 0;
-	for (j = 0; j < 5; ++j)
-	for (i = 0; i < 4800; ++i)
-	{
-		items[j]->feature[i] = i + j;
-	}
-	items[0]->imgID = 0;
-	items[0]->label = 0;
-	items[1]->imgID = 1;
-	items[1]->label = 0;
-	items[2]->imgID = 2;
-	items[2]->label = 0;
-	items[3]->imgID = 3;
-	items[3]->label = 1;
-	items[4]->imgID = 4;
-	items[4]->label = 1;
+  int i = 0;
+  for (j = 0; j < 5; ++j)
+    for (i = 0; i < 4800; ++i)
+    {
+      items[j]->feature[i] = i + j;
+    }
+  items[0]->imgID = 0;
+  items[0]->label = 0;
+  items[1]->imgID = 1;
+  items[1]->label = 0;
+  items[2]->imgID = 2;
+  items[2]->label = 0;
+  items[3]->imgID = 3;
+  items[3]->label = 1;
+  items[4]->imgID = 4;
+  items[4]->label = 1;
 
-	// Create a person
-	Person *person0 = new_person(3);
-	person0->label = 0;
-	person0->items[0] = items[0];
-	person0->items[1] = items[1];
-	person0->items[2] = items[2];
+  // Create a person
+  Person *person0 = new_person(3);
+  person0->label = 0;
+  person0->items[0] = items[0];
+  person0->items[1] = items[1];
+  person0->items[2] = items[2];
 
-	Person *person1 = new_person(2);
-	person1->label = 1;
-	person1->items[0] = items[3];
-	person1->items[1] = items[4];
+  Person *person1 = new_person(2);
+  person1->label = 1;
+  person1->items[0] = items[3];
+  person1->items[1] = items[4];
 
 
-	// create a gallery
-	Gallery *gallery = new_gallery(2);
-	gallery->persons[0] = person0;
-	gallery->persons[1] = person1;
+  // create a gallery
+  Gallery *gallery = new_gallery(2);
+  gallery->persons[0] = person0;
+  gallery->persons[1] = person1;
 
-	// create a database 
-	// in fact, its the content length of GItem
-	int len = sizeof(int)*2 + sizeof(double)*4800;
-	struct db* gdb = createdb(123, "gallery.db", len);
-	if (!gdb)
-	{
-		fprintf(stderr, "can not create databse!\n");
-		return ;
-	}
+  // create a database 
+  // in fact, its the content length of GItem
+  int len = sizeof(int)*2 + sizeof(double)*4800;
+  struct db* gdb = createdb(123, "gallery.db", len);
+  if (!gdb)
+  {
+    fprintf(stderr, "can not create databse!\n");
+    return ;
+  }
 
-	create_gallerydb(gdb, gallery);
-	writedb(gdb);
-	free_gallery(gallery);
+  create_gallerydb(gdb, gallery);
+  writedb(gdb);
+  free_gallery(gallery);
 }
 
 void test_load_gallery()
 {
-	struct db* gdb = fast_opendb("gallery.db");
-	if (!gdb)
-	{
-		fprintf(stderr, "can not open gallery db!\n");
-		return ;
-	}
+  struct db* gdb = fast_opendb("gallery.db");
+  if (!gdb)
+  {
+    fprintf(stderr, "can not open gallery db!\n");
+    return ;
+  }
 
-	Gallery *gallery = open_gallery(gdb);
-	if (!gallery)
-	{
-		fprintf(stderr, "can not construct gallery from your DB!\n");
-		fast_closedb(gdb);	
-		return ;
-	}
+  Gallery *gallery = open_gallery(gdb);
+  if (!gallery)
+  {
+    fprintf(stderr, "can not construct gallery from your DB!\n");
+    fast_closedb(gdb);	
+    return ;
+  }
 	
-	printf("%d\n%d\n", gallery->n, gallery->persons[0]->n);
+  printf("%d\n%d\n", gallery->n, gallery->persons[0]->n);
 
-	for (int i = 0; i < 80; ++i)
-	{
-		printf("%lf ", gallery->persons[1]->items[1]->feature[i]);
-	}
+  for (int i = 0; i < 80; ++i)
+  {
+    printf("%lf ", gallery->persons[1]->items[1]->feature[i]);
+  }
 
-	printf("\n");
+  printf("\n");
 
-	close_gallery(gallery);
+  close_gallery(gallery);
 	
-	fast_closedb(gdb);
+  fast_closedb(gdb);
 
 }
 int main(int argc, char *argv[])
@@ -185,4 +185,6 @@ int main(int argc, char *argv[])
 
   printf("%f", t/1000.);
   getchar();
+
+  return 0;
 }
